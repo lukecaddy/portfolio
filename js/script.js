5b1e30de8dd67fe7d0b3cb5256d7d238de1d4931
Luke_Caddy <lukecaddy@gmail.com>
2018-03-03 10:35:51 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Navbar 
+
+$(document).ready(function() {
+  $('.drawer').drawer();
+}); 
+
+
+// Section animator
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1100);
+    }
+});
